98a74d56257315107074c6b11a423e852efefca6
JonathanTaylor <jonathanmtayor@sbcglobal.net>
2017-06-22 10:22:19 -0400

    first thing

---
+++ JavaScript/0.06_functions.js
@@ -0,0 +1,49 @@
+// function
+
+// declaration
+function hello(){
+	console.log("hallo");
+}
+
+hello();
+
+var hey = function(){
+	console.log("hey");
+}
+
+hey();
+
+function name(){
+	console.log("Jonathan");
+}
+
+name();
+
+function four(){
+	var x = 2
+	var y = 2
+	console.log(x + y);
+}
+
+four();
+
+function multiple(){
+	var u = 3
+	var i = 12
+	console.log(u * i);
+}
+
+multiple();
+
+function numberEntered(x){
+	console.log("The number entered is: ", x);
+}
+numberEntered(5);
+numberEntered(7);
+numberEntered(25);
+
+function anyNumber(x,y){
+	console.log("Sum is: ", x + y);
+}
+
+anyNumber(3,7);
