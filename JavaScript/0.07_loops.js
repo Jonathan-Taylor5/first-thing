54d015db1f16eb75f1c51f0b9a8e0b89cb97ecf0
JonathanTaylor <jonathanmtayor@sbcglobal.net>
2017-06-22 11:34:46 -0400

    first thing

---
+++ JavaScript/0.07_loops.js
@@ -0,0 +1,46 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+for (var i = 1; i < 101; i++) {
+	console.log(i);
+}
+
+for (var i = 5; i < 11; i++) {
+	console.log(i);
+}
+
+for (var i = 10; i > 0; i--) {  
+	console.log(i);
+}
+
+//infinite loop remove comments to activate
+// for (var i = 10; i > 1; i++) {
+// 	console.log(i);
+// }
+
+
+// i+=x adds x to i
+for (var i = 3; i < 30; i+=5) {
+	console.log(i);
+}
+
+for (var x = 0; x < 46; x+=5) {
+	console.log(x);
+}
+console.log("Fifty");
+for (var x = 55; x < 101; x+=5) {
+	console.log(x);
+}
+
+for (var x = 0; x < 46; x+=5) {
+	console.log(x);
+}
+if (x = 45) {
+	console.log("Fifty");
+	for (var x = 55; x < 101; x+=5) {
+	console.log(x);
+}
+}
