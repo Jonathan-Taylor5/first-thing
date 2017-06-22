e807dbb3d9771a85ac74c4a813c299167396dc39
JonathanTaylor <jonathanmtayor@sbcglobal.net>
2017-06-22 13:14:19 -0400

    first thing

--- JavaScript/0.08_Arrays.js
+++ JavaScript/0.08_Arrays.js
@@ -1,6 +1,12 @@
-//             0     1          2            3             4
+//            0      1     2              3           4
 var games = ["MW2", "BO", "Garry's Mod", "R6:Siege", "War Thunder"];
 console.log(games[1]);
 
 var mixedArray = ["dounuts", 20, false];
 console.log(mixedArray[1]);
+
+var favRestuarant = ["Cheesecake Factory", "Noodles & Company", "Bucca di Beppos"]
+console.log(favRestuarant);
+
+
+
