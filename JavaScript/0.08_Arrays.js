65298fbaede05fb2c3b895b1f53d9e677c37367e
JonathanTaylor <jonathanmtayor@sbcglobal.net>
2017-06-22 11:49:47 -0400

    first thing

---
+++ JavaScript/0.08_Arrays.js
@@ -0,0 +1,6 @@
+//             0     1          2            3             4
+var games = ["MW2", "BO", "Garry's Mod", "R6:Siege", "War Thunder"];
+console.log(games[1]);
+
+var mixedArray = ["dounuts", 20, false];
+console.log(mixedArray[1]);
