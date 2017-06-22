12475c22ef331ded62bde6bf6b0624809a1fb2c6
JonathanTaylor <jonathanmtayor@sbcglobal.net>
2017-06-22 08:32:33 -0400

    first thing

---
+++ JavaScript/0.05_conditionals.js
@@ -0,0 +1,19 @@
+conditionals
+
+var age = 16;
+
+if (age === 40) {
+	console.log("Man you are so cool");
+}
+
+if (age === 16) {
+	console.log("Get a job you scrub");
+}`	
+
+var password = "Letmeinbih1245";
+
+if (var password = Letmeinbih1245) {
+	console.log("welcome");
+} else {
+	console.log("You can fight me fam. 1v1 me on rust quickscopes only");
+}
